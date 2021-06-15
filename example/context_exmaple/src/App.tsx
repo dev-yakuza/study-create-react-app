7661e37f5f019b51a7e73ac74067c184a63664c8
dev-yakuza <dev.yakuza@gmail.com>
2021-06-15 14:52:51 +0900

    Add example

---
+++ example/context_exmaple/src/App.tsx
@@ -0,0 +1,15 @@
+import { CountProvider } from './Contexts/Count';
+
+import { CountLabel } from './Components/CountLabel';
+import { PlusButton } from './Components/PlusButton';
+
+function App() {
+  return (
+    <CountProvider>
+      <CountLabel />
+      <PlusButton />
+    </CountProvider>
+  );
+}
+
+export default App;
